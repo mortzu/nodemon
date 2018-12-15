b6baac0cb87e8551931d2fb397123c3009eaa62a
mortzu <mortzu@gmx.de>
2018-12-15 18:27:18 +0100

    Use composer

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ POSSIBILITY OF SUCH DAMAGE.
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" crossorigin="anonymous" integrity="sha256-eSi1q2PG6J7g7ib17yAaWMcrr5GrtohYChqibrV7PBE=">
     <title>Nodewatcher - Freifunk Bremen</title>
   </head>
 
